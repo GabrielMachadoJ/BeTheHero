5ed0adf46258e0f47be8f8f02b8998e5100748e6
Gabriel Machado <gabrielmjj5@gmail.com>
2022-06-14 22:30:44 -0300

    criação da tela de cadastrar ong

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Route, Routes} from 'react-router-dom';
 import Logon from './pages/Logon';
 import Register from './pages/Register';
 import Profile from './pages/Profile'
+import NewIncident from './pages/NewIncident'
 
 export default function Rotas() {
     return (
@@ -13,6 +14,7 @@ export default function Rotas() {
                 <Route path="/register" element={<Register />} />
 
                 <Route path="/profile" element={<Profile />} />
+                <Route path="/incidents/new" element={<NewIncident />} />
             </Routes>
         </BrowserRouter>
     )
